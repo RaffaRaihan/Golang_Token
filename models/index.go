63c71dac7dbdf9aed20abf5b5ee8750bd0a96c3b
Kaizy Exercise <146798505+RaffaRaihan@users.noreply.github.com>
2024-09-09 13:50:58 +0700

    "Mengubah MySQL Menjadi Postgres"

--- models/index.go
+++ models/index.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 	"os"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func GetSqlConnection() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Asia%%2FJakarta",
-		os.Getenv("MYSQL_USER"),
-		os.Getenv("MYSQL_PASS"),
-		os.Getenv("MYSQL_HOST"),
-		os.Getenv("MYSQL_PORT"),
-		os.Getenv("MYSQL_DB"),
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
+		os.Getenv("PG_HOST"),   // Urutan variabel host
+		os.Getenv("PG_USER"),   // Urutan variabel user
+		os.Getenv("PG_PASS"),   // Urutan variabel password
+		os.Getenv("PG_DB"),     // Urutan variabel dbname
+		os.Getenv("PG_PORT"),   // Urutan variabel port
 	)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		return nil, err
